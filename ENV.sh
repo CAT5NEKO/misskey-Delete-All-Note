8740f9387944d89e5d72f904ca9e689bca8f2469
CAT5NEKO <JFKnet1984@gmail.com>
2024-03-31 09:35:35 +0900

    シェルを破壊されたので修正

--- ENV.sh
+++ ENV.sh
@@ -7,15 +7,15 @@ read token
 echo "定期実行する時刻を24時間形式で入力してください（例：23:59）:"
 read post_time
 
-echo "TOKEN=$token" >.env
-echo "HOST=$host" >>.env
+echo "TOKEN=$token" > .env
+echo "HOST=$host" >> .env
 
-post_hour=$(echo "$post_time" | cut -d: -f1)
-post_minute=$(echo "$post_time" | cut -d: -f2)
+post_hour=$(echo $post_time | cut -d: -f1)
+post_minute=$(echo $post_time | cut -d: -f2)
 
-current_dir=$PWD
+current_dir=$(pwd)
 
 echo "以下のコマンドをcrontabに追加してください:"
-echo "$post_minute $post_hour * * * cd $current_dir && ./misskeyNotedel"
+echo "$post_minute $post_hour * * * cd $current_dir && ./misskeyJihou"
 
 echo "設定が完了しました。"
